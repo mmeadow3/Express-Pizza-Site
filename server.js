fdb7a84b6c4f1d4c5d981d334cfc35e0a283851b
Mike Meadows <mmeadow33@gmail.com>
2016-09-08 13:19:03 -0500

    added routing to project

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const app = express(); /////creates instance of express server
 const bodyParser = require("body-parser"); ////npm installed
 const { cyan, red } = require('chalk') ////just for some styling
 
+const routes = require("./routes/")
+
 // PORT=1337 node server.js   in terminal to set temporary port
 const port = process.env.PORT || 3000
 app.set("port", port)
@@ -37,23 +39,11 @@ app.use((req, res, next) => {
 //   res.send(`<h1>Welcome to my app</h1>`)
 // )
 ///////http localhost:3000 //// can run this in terminal to find out what text says
-app.get('/', function (req, res) { //////getting the requested page
-  res.render('index');  //////what will send on the page
-  })
-.get("/about", function (req, res){
-  res.render("about", {title: "about"})
-})
-.get("/contact", function (req, res){
-  res.render("contact", {title: "contact"})
-})
-.post("/contact", function (req, res){
-  console.log(req.body);
-  // res.send("Thanks for contacting us")
-  res.redirect("/")
-})
-app.get("/404", function (req, res){
-  res.render("404") //////send page that contains 404 error message
-})
+
+////////routes///////
+app.use(routes)  ////////now in the routes/index.js
+//////////////
+
 
 /////error handling needs to go below the piping routes////////
 ///////error handling for 404 errors/////////
