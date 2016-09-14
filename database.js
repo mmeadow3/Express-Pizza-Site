66bd1c707275c831decc964a1dc54566bb396eb8
Mike Meadows <mmeadow33@gmail.com>
2016-09-14 09:35:14 -0500

    removed mongo, added mongoose

--- database.js
+++ database.js
@@ -1,10 +1,19 @@
 "use strict";
 
-const { MongoClient: { connect } } = require("mongodb");
+const mongoose = require("mongoose");
+
 const MONGODB_URL = "mongodb://localhost:27017/pugspizza"  //////need to make a new db every project
+// let db    //////declare db here, then rename it below so it can be reused
 
-let db    //////declare db here, then rename it below so it can be reused
+
+mongoose.Promise = Promise
+
+module.exports.connect = () => mongoose.connect(MONGODB_URL)
+// .then(mydb => db = mydb)
 
 
-module.exports.connect = () => connect(MONGODB_URL).then(mydb => db = mydb)
-module.exports.db = () => db
+
+
+
+
+// module.exports.db = () => db
